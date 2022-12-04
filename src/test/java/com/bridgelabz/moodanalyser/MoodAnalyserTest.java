50ce5e0ca50effffdcc3e50bcd7fd294bcfbb7e3
urana03 <uhackninja@gmail.com>
2022-12-04 15:16:31 +0530

    Add:Test case given any mood return happy

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -3,15 +3,14 @@ package com.bridgelabz.moodanalyser;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-import org.junit.Test;
-
 public class MoodAnalyserTest {
 
+	 
 	 @Test
-	    public void given_SadMood_Should_Return_Sad() {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser();
-	        String mood = moodAnalyser.analyseMood("I am in SAD");
-	        Assert.assertEquals("SAD", mood);
-
-	 }
+		public void given_AnyMood_Should_Return_HAPPY() {
+			MoodAnalyser moodAnalyser = new MoodAnalyser();
+			String mood = moodAnalyser.analyseMood("I am in any Mood");
+			Assert.assertEquals("HAPPY", mood);
+	  }
+	 
 }
