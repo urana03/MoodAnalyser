a478f8c5ea39f69e8cc6c94c4aeb50f6bd9b9328
urana03 <uhackninja@gmail.com>
2022-12-04 15:35:03 +0530

    Add:Test code with parameterized constrctor to check sad mood

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -3,14 +3,13 @@ package com.bridgelabz.moodanalyser;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+
 public class MoodAnalyserTest {
 
-	 
 	 @Test
-		public void given_AnyMood_Should_Return_HAPPY() {
-			MoodAnalyser moodAnalyser = new MoodAnalyser();
-			String mood = moodAnalyser.analyseMood("I am in any Mood");
-			Assert.assertEquals("HAPPY", mood);
-	  }
-	 
+	    public void given_SadMood_Should_Return_Sad() {
+	        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in SAD");
+	        String mood = moodAnalyser.analyseMood();
+	        Assert.assertEquals("SAD", mood);
+	 }	 
 }
