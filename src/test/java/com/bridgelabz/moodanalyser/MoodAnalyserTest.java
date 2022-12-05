4c996abc70f987232cc30af6f39a194465fe2501
urana03 <uhackninja@gmail.com>
2022-12-05 10:40:13 +0530

    Add:Test case to use custom exception to throw a message

--- src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -2,14 +2,23 @@ package com.bridgelabz.moodanalyser;
 
 import static org.junit.Assert.*;
 import org.junit.*;
+import org.junit.rules.ExpectedException;
 
 
 public class MoodAnalyserTest {
 
-	 @Test
+	    @Test
 	    public void givenNullMoodShouldReturnHappy() {
 	        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-	        String mood = moodAnalyser.analyseMood();
+	        String mood = null;
+	        try {
+	        	ExpectedException exceptionRule = ExpectedException.none();
+	        	exceptionRule.expect(MoodAnalysisException.class);
+	        	mood = moodAnalyser.analyseMood();
 	        Assert.assertEquals("HAPPY", mood);
+	        }
+	        catch(MoodAnalysisException e) {
+	        	e.printStackTrace();
+	        }
 	 }	 
 }
